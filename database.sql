a617121e1021aa471c759b106fe1bdb836460308
ThiNgocPhuong <105435187+ThiNgocPhuong@users.noreply.github.com>
2022-11-15 11:57:47 +0100

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,9 @@
+insert into Utilisateur (
+    email, nom, prenom, mdp
+)
+values
+('noble@gmail.com', 'Criquet', 'Jean-Michel', 'Mabeaute=monintelligence'),
+('salle.danse@gmail.com','Antonova', 'Mihaela','Jesuisunepisseuse'),
+('jardinàgmail.com', 'Sam', 'Caroline','jesuis@ccroausucre'),
+('salle.bain@gmail.com', 'Bouba', 'Pingu', 'jesuisunminiNicolaen+beau'),
+('fetedelasaucice@gmail.com', 'Saucisse', 'Saucisse','Jesuisunebonnes@ucice');

4d0321d20c703a6ad1f1dfacc69766df2837f9b0
ThiNgocPhuong <105435187+ThiNgocPhuong@users.noreply.github.com>
2022-11-15 13:36:13 +0100

    Add files via upload

--- database.sql
+++ database.sql
@@ -1,9 +1,29 @@
-insert into Utilisateur (
+INSERT INTO Utilisateur (
     email, nom, prenom, mdp
 )
-values
+VALUES
 ('noble@gmail.com', 'Criquet', 'Jean-Michel', 'Mabeaute=monintelligence'),
 ('salle.danse@gmail.com','Antonova', 'Mihaela','Jesuisunepisseuse'),
-('jardinàgmail.com', 'Sam', 'Caroline','jesuis@ccroausucre'),
+('jardin@gmail.com', 'Sam', 'Caroline','jesuis@ccroausucre'),
 ('salle.bain@gmail.com', 'Bouba', 'Pingu', 'jesuisunminiNicolaen+beau'),
 ('fetedelasaucice@gmail.com', 'Saucisse', 'Saucisse','Jesuisunebonnes@ucice');
+
+INSERT INTO Parametre_Concours (participation_debut,participation_fin)
+VALUES( "2023-01-09"),("2023-02-12");
+
+INSERT INTO Gerant(email)
+VALUES('fetedelasaucice@gmail.com');
+
+INSERT INTO Participant(email)
+VALUES('noble@gmail.com'),
+('salle.danse@gmail.com'),
+('jardinàgmail.com'),
+('salle.bain@gmail.com');
+
+insert into Realisation(id_realisation, photo, date_debut_realisation, date_fin_realisation, date_participation, email)
+values(01,'C:\Users\SAM\photos_projet','2023-01-10','2023-01-30', '2023-02-01','noble@gmail.com'),
+(02,'C:\Users\SAM\photos_projet','2023-01-10','2023-01-20', '2023-01-30','salle.danse@gmail.com'),
+(03,'C:\Users\SAM\photos_projet','2023-01-01','2023-01-20', '2023-01-29','jardin@gmail.com'),
+(04,'C:\Users\SAM\photos_projet','2023-01-03','2023-01-26', '2023-02-09','salle.bain@gmail.com');
+
+

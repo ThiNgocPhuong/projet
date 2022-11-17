8d39ba18638e142c499de60a3fc8da0564d90b55
Mihaela Antonova <98533156+mihaela0506@users.noreply.github.com>
2022-11-17 09:01:26 +0100

    Update formulaire_photo.php

--- formulaire_photo.php
+++ formulaire_photo.php
@@ -1,35 +1,41 @@
+<!--
+    ANTONOVA & SAM le 17/11/2022
+    style.css
+    CSS pour le formulaire du projet de Ged'Imagination
+-->
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="stylephp.css" rel="stylesheet" type="text/css">
         <title>Formulaire</title>
     </head>
     <body>
         <form>
             <h1>Formulaire</h1>
-            <br>
-            <label for="titre">Titre : </label>
-            <input type="text" name="titre" id="titre">
-            <br>
-            <label for="Description">Description : </label>
-            <input type="text"  id="Description" name="description" maxlength="2000">
-            <br>
-            <label for="debut">Début de la réalisation : </label>
-            <input type="date" name="debut" id="debut">
-            <br>
-            <label for="fin">Fin de la réalisation : </label>
-            <input type="date" name="fin" id="fin">
-            <br>
-            <label for="photo">Photo : </label>
-            <input type="file" name="photo" id="photo" accept="image/png, image/jpeg">
-            <br>
-            <input type="button" value="Valider !">
+            <!--Conteneur du formulaire-->
+            <div id="conteneur">
+                <br>
+                <br>
+                <label for="titre">Titre : </label> <br>
+                <input type="text"  id="titre">
+                <br>
+                <label for="Description">Description : </label> <br>
+                <input type="text"  id="Description" maxlength="2000">
+                <br>
+                <label for="realisationD">Début de la réalisation : </label> <br>
+                <input type="date"  id="realisationD">
+                <br>
+                <label for="realisationF">Fin de la réalisation : </label> <br>
+                <input type="date" id="realisationF">
+                <br>
+                <label for="Photo">Photo : </label> <br>
+                <input type="file" id="Photo" accept="image/png, image/jpeg">
+                <br>
+                <input type="button" value="Valider !">
         </form>
-
-        <?php
-        
-        ?>
     </body>
 </html>

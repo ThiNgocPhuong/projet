31dd4641368f10ddc45ad36fb53b9400ff0c3ea8
ThiNgocPhuong <105435187+ThiNgocPhuong@users.noreply.github.com>
2022-11-17 16:49:10 +0100

    Add files via upload

--- db.inc.projet.php
+++ db.inc.projet.php
@@ -2,7 +2,7 @@
     define('SGBDR', 'mysql');
     define('HOST', 'localhost');
     define('PORT', '3306');
-    define('LOGIN', 'root');
-    define('PASSW', '');
+    define('LOGIN', 'Participant');
+    define('PASSW', 'C0nc0urs*');
     define('BDD', 'GEDIMAGINATION');
 ?>

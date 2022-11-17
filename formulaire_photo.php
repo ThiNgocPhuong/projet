a1e9616aafd75ccc1429ce32062e0538f910d1c6
Mihaela Antonova <98533156+mihaela0506@users.noreply.github.com>
2022-11-17 11:17:04 +0100

    Update formulaire_photo.php

--- formulaire_photo.php
+++ formulaire_photo.php
@@ -5,7 +5,7 @@
 -->
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,37 +16,37 @@
     <body>
         <div id="conteneur">
             <form action="traitement.php" method="post">
-                <h1>Formulaire</h1>
+                <h1>Formulaire de participation</h1>
                 <!--Conteneur du formulaire-->
                     <div id="titre">
                         <label for="titre">Titre : </label> <br>
-                        <input type="text" name="titre" id="titre">
+                        <input type="text" name="titre" id="titre" placeholder="Veuillez écrire le titre de votre réalisation">
                     </div>
                     <div id="description">
                         <label for="description">Description : </label><br>
-                        <input type="text" name="description" id="description" maxlength="2000">
+                        <textarea id="description" name="description" rows="6" placeholder="Veuillez écrire la description de votre réalisation"></textarea>
                     </div>
                     <div id="debut">
-                        <label for="debut">Début de la réalisation : </label> <br>
+                        <label for="debut">Début de la réalisation : </label> 
                         <input type="date" name="debut" id="debut">
                     </div>
                     <div id="fin">
-                        <label for="fin">Fin de la réalisation : </label> <br>
+                        <label for="fin">Fin de la réalisation : </label> 
                         <input type="date" name="fin" id="fin">
                     </div>
                     <div id="photo">
                         <label for="Photo">Photo : </label> <br>
                         <input type="file" id="Photo" accept="image/png, image/jpeg">
                     </div>
-                    <div>
+                    <div id="date">
                         <p> Date de participation : 
                         <script>
                         date = new Date().toLocaleDateString();
                         document.write(date);
                         </script> </p>
                     </div>
-                        <div class="bouton">
-                        <input type="submit" >
+                        <div id="bouton" class="bouton">
+                        <input type="submit" value="Valider !">
                 </div>
             </form>
         </div>

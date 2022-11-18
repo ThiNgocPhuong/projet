66b4beb95e47dba6cdb6d4e66e26a2af921f9787
ThiNgocPhuong <105435187+ThiNgocPhuong@users.noreply.github.com>
2022-11-18 10:13:03 +0100

    Update insert_projet.php

--- insert_projet.php
+++ insert_projet.php
@@ -8,7 +8,7 @@
         $objetPDO= new PDO('mysql:host=localhost;dbname=GEDIMAGINATION', $user, $pass);
 
         //préparation de la requête
-        $requete=$objetPDO->prepare('INSERT INTO Realisation (photo, titre_realisation,description_realisation, date_debut_realisation, date_fin_realisation)  VALUES (:photo, :titre, :description, :debut, :fin)');
+        $requete=$objetPDO->prepare('INSERT INTO Realisation (photo, titre_realisation,description_realisation, date_debut_realisation, date_fin_realisation, date_participation)  VALUES (:photo, :titre, :description, :debut, :fin, :date)');
 
         //liaison de chaque valeur
         $requete->bindValue(':photo', $_POST['photo'], PDO::PARAM_STR);
@@ -16,6 +16,7 @@
         $requete->bindValue(':description', $_POST['description'], PDO::PARAM_STR);
         $requete->bindValue(':debut', $_POST['debut'], PDO::PARAM_STR);
         $requete->bindValue(':fin', $_POST['fin'], PDO::PARAM_STR);
+        $requete->bindValue(':date', date("Y-m-d"));  
 
         //exécution de la requette préparer
         $insertValid = $requete->execute();

a3dab6b2991fab92825443373568cd3bc6df593d
ThiNgocPhuong <105435187+ThiNgocPhuong@users.noreply.github.com>
2022-11-18 10:40:43 +0100

    Delete db.inc.projet.php

--- db.inc.projet.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define('SGBDR', 'mysql');
-    define('HOST', 'localhost');
-    define('PORT', '3306');
-    define('LOGIN', 'Participant');
-    define('PASSW', 'C0nc0urs*');
-    define('BDD', 'GEDIMAGINATION');
-?>

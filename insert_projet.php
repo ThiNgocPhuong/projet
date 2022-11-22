48ad07d48a7dfc4de861ab08a9d5817e122447f9
ThiNgocPhuong <105435187+ThiNgocPhuong@users.noreply.github.com>
2022-11-22 13:56:59 +0100

    Add files via upload

--- insert_projet.php
+++ insert_projet.php
@@ -3,15 +3,49 @@
     $user="root";
     $pass="";
 
+    //Vérification si le formulaire a été soumis
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        //Vérification du fichier s'il a été upload sans erreurs
+        if(isset($_FILES["photo"]) && $_FILES["photo"]["error"]==0){
+            $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "png" => "image/png");
+            $filename = $_FILES["photo"]["name"];
+            $filetype = $_FILES["photo"]["type"];
+            $filesize = $_FILES["photo"]["size"];
+
+            //Vérification de l'extension de la photo
+            $ext = pathinfo($filename, PATHINFO_EXTENSION);
+            if(!array_key_exists($ext,$allowed)) die("Erreur : Veuillez séléctionner un format de fichier valide.");
+
+            //Vérification de la taille de la photo (max 10Mo)
+            $maxsize = 9 * 2048 * 1536;
+            if($filesize > $maxsize) die("Error : La taille du fichier est supérieure à la taille autorisée.");
+
+            //Vérification du type - MIME de la photo
+            if(in_array($filetype, $allowed)){
+                //Vérification de l'existense de la photo avant de la télécharger
+                if(file_exists("C:\laragon\www\projet\photos/" . $_FILES["photo"]["name"])){
+                    echo $_FILES["photo"]["name"] . " existe déjà.";
+                    }
+                    else {
+                        move_uploaded_file($_FILES["photo"]["tmp_name"], "C:\laragon\www\projet\photos/" . $_FILES["photo"]["name"]);
+                        echo "Votre image a été télérchagée avec succès";
+                    }
+            } else {
+                echo "Error: Il y a eu un problème de téléchargement de votre image. Veuillez réesayer!";
+            }
+        } else {
+            echo "Error: " . $_FILES["photo"]["error"];
+        }
+    }
+
     try
     {
         $objetPDO= new PDO('mysql:host=localhost;dbname=gedimagination', $user, $pass);
 
         //préparation de la requête
-        $requete=$objetPDO->prepare('INSERT INTO Realisation (photo, titre_realisation,description_realisation, date_debut_realisation, date_fin_realisation, date_participation)  VALUES (:photo, :titre, :description, :debut, :fin, :date)');
+        $requete=$objetPDO->prepare('INSERT INTO Realisation ( titre_realisation,description_realisation, date_debut_realisation, date_fin_realisation, date_participation)  VALUES ( :titre, :description, :debut, :fin, :date)');
 
         //liaison de chaque valeur
-        $requete->bindValue(':photo', $_POST['photo'], PDO::PARAM_STR);
         $requete->bindValue(':titre', $_POST['titre'], PDO::PARAM_STR);
         $requete->bindValue(':description', $_POST['description'], PDO::PARAM_STR);
         $requete->bindValue(':debut', $_POST['debut'], PDO::PARAM_STR);
@@ -59,3 +93,6 @@
     </div>
 </body>
 </html>
+
+
+</script>

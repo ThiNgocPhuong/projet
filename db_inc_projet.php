09495f1d871342620e11ae9473955b54f733afd8
Mihaela Antonova <98533156+mihaela0506@users.noreply.github.com>
2022-11-24 13:14:56 +0100

    Add files via upload

---
+++ db_inc_projet.php
@@ -0,0 +1,8 @@
+<?php
+    define('SGBDR', 'mysql');
+    define('HOST', 'localhost');
+    define('PORT', '3306');
+    define('LOGIN', 'root');
+    define('PASSW', '');
+    define('BDD', 'GEDIMAGINATION');
+?>

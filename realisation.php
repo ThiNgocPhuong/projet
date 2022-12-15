154aea9523caac88a73b21ffcc0f501501273cd5
ThiNgocPhuong <105435187+ThiNgocPhuong@users.noreply.github.com>
2022-12-15 08:16:57 +0100

    Add files via upload

--- realisation.php
+++ realisation.php
@@ -1,6 +1,7 @@
 <?php
     include_once('fonctionDAL.inc.php');
-    try{
+    try
+    {
         $request_method = $_SERVER["REQUEST_METHOD"];
         switch($request_method)
         {
@@ -15,7 +16,8 @@
 				break;
         }
     }
-    catch (Exception $e) {
+    catch (Exception $e) 
+    {
 		echo $e->getMessage();
 		http_response_code(500);
 	}
@@ -25,30 +27,36 @@
 		$lesRealisation = DAL_getRealisation();
 		echo json_encode($lesRealisation);
 	}
-    function putRealisation(){
+    function putRealisation()
+    {
 
         $json =  file_get_contents('php://input');
         $errJson=true;
         if(!empty($json)){
             $obj = json_decode($json);
-            if (property_exists($obj,"Realisation")){
+            if (property_exists($obj,"Realisation"))
+            {
                 $res = $obj->Reaisation;
                 $errUpdate = false;
-                foreach($res as $uneRealisation){
+                foreach($res as $uneRealisation)
+                {
                     if(property_exists($uneRealisation,"id_realisation")&& 
                     property_exists($uneRealisation,"titre_realisation")&&
-                    property_exists($uneRealisation,"description_realisaton")){
+                    property_exists($uneRealisation,"description_realisaton"))
+                    {
                         $id = $uneRealisation->id_realisation;
                         $titre = $uneRealisation->titre_realisation;
                         $description = $uneRealisation->description_realisation;
                         $retourUpdate = DAL_InsertSQLiteDatabase($id,$titre,$description);
-                        if($retourUpdate == 1){
+                        if($retourUpdate == 1)
+                        {
                             $message .= "Realisation" .$id. "ajouter avec succès.";
                         }
                         $errJson=false;
                     }
                 }
-                if($errUpdate == false){
+                if($errUpdate == false)
+                {
                     header('Content-Type: application/json');
 					echo json_encode($messageInfo);
 					http_response_code(200);

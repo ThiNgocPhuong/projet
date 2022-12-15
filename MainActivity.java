1c2f6dc9f6fada7732e2e28e6906337971d265a1
ThiNgocPhuong <105435187+ThiNgocPhuong@users.noreply.github.com>
2022-12-15 10:27:31 +0100

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,4 +1,6 @@
 package com.example.gedimagination;
+import static com.loopj.android.http.AsyncHttpClient.log;
+
 import androidx.appcompat.app.AppCompatActivity;
 // inclusion des packages nécessaires
 import android.content.Intent;
@@ -31,32 +33,32 @@ public class MainActivity extends AppCompatActivity {
     public View.OnClickListener EcouteurBouton = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-           //Requête HTTP GET
-           String urlI= "http://localhost/projet/PHPprojet/realisation.php";
+            //Requête HTTP GET
+            String urlI= "http://localhost/projet/PHPprojet/realisation.php";
             AsyncHttpClient requestI = new AsyncHttpClient();
             requestI.get(urlI, new JsonHttpResponseHandler() {
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
                     super.onSuccess(statusCode, headers, response);
 
-                    bdd = new ConcoursDAO(MainActivity.this);
-                    for (int i=0; i < response.length(); i++){
-                        try{
+                    ConcoursDAO bdd = new ConcoursDAO(MainActivity.this);
+                    for (int i = 0; i < response.length(); i++) {
+                        try {
                             String id = response.getJSONObject(i).getString("id_realisation");
                             String titre = response.getJSONObject(i).getString("titre_realisation");
-                            String description = response.getJSONObject(i).getString("description_realisation")
+                            String description = response.getJSONObject(i).getString("description_realisation");
                             Realisation R = new Realisation();
                             R.setId(id);
                             R.setTitre(titre);
                             R.setDescription(description);
-                            log.i("info",R.toString());
+                            log.i("info", R.toString());
                             bdd.ajouterRealisation(R);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
-
+                }
+            });
         }
-
     };
 }

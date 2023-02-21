0deba8bab7e63e330aad3a41be6dcfbe40303cc3
ThiNgocPhuong <105435187+ThiNgocPhuong@users.noreply.github.com>
2023-02-21 13:42:04 +0100

    Update ConcoursHelper.java

--- ConcoursHelper.java
+++ ConcoursHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class ConcoursHelper extends SQLiteOpenHelper{
     public ConcoursHelper(Context context){
-        super(context,"baseConcour.db", null,1);
+        super(context,"baseConcour.db", null,2);
     }
 
     @Override

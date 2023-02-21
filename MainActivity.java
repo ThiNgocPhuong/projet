a605477ccb2eaf725c090dc93080cd98acbf2e8c
ThiNgocPhuong <105435187+ThiNgocPhuong@users.noreply.github.com>
2023-02-21 13:41:17 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -38,63 +38,67 @@ public class MainActivity extends AppCompatActivity {
         btnExport.setOnClickListener(EcouteurBouton);
 
         Date actuel = Calendar.getInstance().getTime();
-        Date debut = new Date(122,11,16);
-        Date fin = new Date(123,12,16);
+        Date debut = new Date(122, 11, 15);
+        Date fin = new Date(123, 12, 30);
 
-        if(actuel.before(debut) && actuel.after(fin)) {
-            btnImporter.setEnabled(true);
-        } else{
-            btnImporter.setEnabled(false);
-        }
+
     }
+
     public View.OnClickListener EcouteurBouton = new View.OnClickListener() {
         @SuppressLint("Range")
         @Override
         public void onClick(View view) {
-            //Requête HTTP GET
-            String urlI= "http://10.0.2.2/projet/PHPprojet/realisation.php";
-            AsyncHttpClient requestI = new AsyncHttpClient();
+            switch (view.getId()) {
+                case R.id.btnImporter:
+                    // Requête HTTP GET
+                    String urlI = "http://10.0.2.2/projet/PHPprojet/realisation.php";
+                    AsyncHttpClient requestI = new AsyncHttpClient();
 
-            requestI.get(urlI, new JsonHttpResponseHandler() {
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                    super.onSuccess(statusCode, headers, response);
-                    log.i("donnée",response.toString());
-                    ConcoursDAO bdd;
+                    requestI.get(urlI, new JsonHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                            super.onSuccess(statusCode, headers, response);
+                            Log.i("donnée", response.toString());
+                            // Deserialisation du flux JSON
+                            ConcoursDAO bdd;
 
-                    bdd = new ConcoursDAO(MainActivity.this);
-                    bdd.supprimerTous();
+                            bdd = new ConcoursDAO(MainActivity.this);
+                            bdd.supprimerTous();
 
-                    for (int i = 0; i < response.length(); i++) {
-                        try {
-                            String id = response.getJSONObject(i).getString("id_realisation");
-                            String titre = response.getJSONObject(i).getString("titre_realisation");
-                            String description = response.getJSONObject(i).getString("description_realisation");
-                            String debut =  response.getJSONObject(i).getString("debut");
-                            String fin = response.getJSONObject(i).getString("fin");
+                            for (int i = 0; i < response.length(); i++) {
+                                try {
+                                    String id = response.getJSONObject(i).getString("id_realisation");
+                                    String titre = response.getJSONObject(i).getString("titre_realisation");
+                                    String description = response.getJSONObject(i).getString("description_realisation");
+                                    String debut = response.getJSONObject(i).getString("date_debut_realisation");
+                                    String fin = response.getJSONObject(i).getString("date_fin_realisation");
 
-                            Realisation R = new Realisation();
-                            R.setId(id);
-                            R.setTitre(titre);
-                            R.setDescription(description);
-                            R.setDebut(debut);
-                            R.setFin(fin);
+                                    Realisation R = new Realisation();
+                                    R.setId(id);
+                                    R.setTitre(titre);
+                                    R.setDescription(description);
+                                    R.setDebut(debut);
+                                    R.setFin(fin);
 
-                            log.i("info", R.toString());
-                            bdd.ajouterRealisation(R);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                                    Log.i("info", R.toString());
+                                    bdd.ajouterRealisation(R);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            Toast.makeText(getApplicationContext(), "Imporation terminée", Toast.LENGTH_LONG).show();
                         }
-                    }
-                    Toast.makeText(getApplicationContext(), "Imporation terminée", Toast.LENGTH_LONG).show();
-                }
-                @Override
-                public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                    super.onFailure(statusCode, headers, responseString, throwable);
-                    Log.i("Erreur", String.valueOf(statusCode) + "Erreur = " + responseString);
-                    Toast.makeText(getApplicationContext(), "Echec de l'importation", Toast.LENGTH_LONG).show();
-                }
-            });
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                            super.onFailure(statusCode, headers, responseString, throwable);
+                            Log.i("Erreur", String.valueOf(statusCode) + "Erreur = " + responseString);
+                            Toast.makeText(getApplicationContext(), "Echec de l'importation", Toast.LENGTH_LONG).show();
+                        }
+                    });
+            }
         }
     };
 }
+
+

5d45cfa934b56f4cff4674fbf7e982cdb109fd45
ThiNgocPhuong <105435187+ThiNgocPhuong@users.noreply.github.com>
2023-05-04 14:08:58 +0200

    Add files via upload

---
+++ db.inc.gestion.php
@@ -0,0 +1,8 @@
+<?php
+    define('SGBDR', 'mysql');
+    define('HOST', 'localhost');
+    define('PORT', '3306');
+    define('LOGIN', 'Gestion');
+    define('PASSW', 'Ger@ant2023');
+    define('BDD', 'GEDIMAGINATION');
+?>
---
+++ db.inc.projet.php
@@ -0,0 +1,8 @@
+<?php
+    define('SGBDR', 'mysql');
+    define('HOST', 'localhost');
+    define('PORT', '3306');
+    define('LOGIN', 'Participation');
+    define('PASSW', 'C0nc0urs*');
+    define('BDD', 'GEDIMAGINATION');
+?>

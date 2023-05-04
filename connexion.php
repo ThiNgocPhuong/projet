7e810ac6f9d0873af14f94a654e3013fbe2f46dd
Mihaela Antonova <98533156+mihaela0506@users.noreply.github.com>
2023-05-04 14:44:07 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -4,19 +4,19 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="style.css" rel="stylesheet" type="text/css">
+        <link href="style_connexion.css" rel="stylesheet" type="text/css">
         <title>Connexion</title>
     </head>
     <body>
         <div id="conteneur">
             <h1>Connexion</h1>
-            <div id="identifiant">
-                <label for="identifiant">E-mail</label>
-                <input type="text" name="identifiant" id="identifiant" placeholder="Veuillez saisir votre identifiant" pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$" required>
+            <div id="email">
+                <label for="email">E-mail</label>
+                <input type="text" name="email" id="email" placeholder="Veuillez saisir votre adresse mail" pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$" required>
             </div>
             <div id="mdp">
                 <label for="mdp">Mot de passe</label>
-                <input type="text" name="mdp" id="mdp" placeholder="Veuillez saisir votre mot de passe" pattern="/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{12,}$/" required>
+                <input type="password" name="mdp" id="mdp" placeholder="Veuillez saisir votre mot de passe" pattern="/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{12,}$/" required>
             </div>
             <div id="bouton" class="bouton">
                 <input type="submit" value="Connexion !">
